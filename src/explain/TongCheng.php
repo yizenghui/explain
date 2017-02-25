fe2466fa35cc475fa7482cca9d7fcbcb33303e21
Administrator <245561237@qq.com>
2017-02-25 16:40:52 +0800

    获取职位列表和详细

--- src/explain/TongCheng.php
+++ src/explain/TongCheng.php
@@ -5,49 +5,79 @@
 
 namespace ExplainHtml;
 use QL\QueryList;
-use phpQuery;
 class TongCheng{
 
+    static protected $job_tag_role = '/<span><i><\/i>([^<]+)<\/span>/';
+
+    // http://mm.58.com/yewu/28953062755900x.shtml
+    static protected $job_item_url_role = '/http:\/\/(\w+).58.com\/(\w+)\/(\d+)x.shtml/';
 
 
-    static public function resumeInfo($html)
+    static public function m_job_role(){
+        return [
+            "title"=>['.d_title','text'],
+            "position"=>['.job_con .attrValue>a:eq(0)','text'],
+            "location"=>['.job_con .dizhiValue','text','',function($content){
+                return trim($content);
+            }],
+            "description"=>['.dis_con p','text'],
+
+            "company_name"=>['.company .c_tit a:eq(0)','html'],
+            "company_url"=>['.company .c_tit a:eq(0)','href'],
+
+//            "mobile"=>['.ffield #contact_phone','phoneno'],
+
+            "pay"=>['.price .pay','text'],
+            "pub_at"=>['.pub_date span:eq(1)','text'],
+            "tag"=>['.fulivalue','html','',function($content){
+                if($content && preg_match_all(self::$job_tag_role,$content,$tags)){
+                    return $tags[1];
+                }
+                return $content;
+            }],
+        ];
+    }
+
+    static public function jobInfo($html)
     {
+        $hj = QueryList::Query($html,self::m_job_role());
+        $html = $hj->getHtml(false);
+        $linkman =  self::match_find($html,1,'/{"I":"5333","V":"(.+?)"},/');
+        $mobile =  self::match_find($html,1,'/id="contact_phone" phoneno="(\d+?)"/');
+        $email =  self::match_find($html,1,'/{"I":"5360","V":"(.+?)"},/');
+        //  $mobile = $data[0];
+        return array_merge($hj->data[0],compact('linkman','email','mobile'));
+    }
 
-//需要采集的目标页面
-$page = 'http://cms.querylist.cc/news/566.html';
-//采集规则
-$reg = array(
-    //采集文章标题
-    'title' => array('h1','text'),
-    //采集文章发布日期,这里用到了QueryList的过滤功能，过滤掉span标签和a标签
-    'date' => array('.pt_info','text','-span -a',function($content){
-        //用回调函数进一步过滤出日期
-        $arr = explode(' ',$content);
-        return $arr[0];
-    }),
-    //采集文章正文内容,利用过滤功能去掉文章中的超链接，但保留超链接的文字，并去掉版权、JS代码等无用信息
-    'content' => array('.post_content','html','a -.content_copyright -script',function($content){
-            //利用回调函数下载文章中的图片并替换图片路径为本地路径
-            //使用本例请确保当前目录下有image文件夹，并有写入权限
-            //由于QueryList是基于phpQuery的，所以可以随时随地使用phpQuery，当然在这里也可以使用正则或者其它方式达到同样的目的
-            $doc = phpQuery::newDocumentHTML($content);
-            $imgs = pq($doc)->find('img');
-            foreach ($imgs as $img) {
-                $src = 'http://cms.querylist.cc'.pq($img)->attr('src');
-                $localSrc = 'image/'.md5($src).'.jpg';
-                $stream = file_get_contents($src);
-                file_put_contents($localSrc,$stream);
-                pq($img)->attr('src',$localSrc);
-            }
-            return $doc->htmlOuter();
-    })
-    );
-$rang = '.content';
-$ql = QueryList::Query($page,$reg,$rang);
-$data = $ql->getData();
-//打印结果
-print_r($data);
-		
-        return [];
+
+    // 从58同城职位列表中获取职位详细数据 (pc版)
+    static public function jobList($html)
+    {
+        $links = QueryList::Query($html,[
+            'link'=>['.infolist dt a','href','',function($link){
+                if( preg_match(self::$job_item_url_role,$link,$val) ){
+                    // 组装成手机版的详细地址 (pc的手机号加密了)
+                    return "http://m.58.com/{$val[1]}/{$val[2]}/{$val[3]}x.shtml";
+                }
+                return false;
+            }]
+        ])->getData(function($item){
+            return $item['link'];
+        });
+        return array_filter($links);
+    }
+
+    /**
+     * 简单的字符匹配
+     * @param $str
+     * @param $i int 取位
+     * @param $i string  正则式
+     */
+    function match_find($str, $i = 1, $rep = '/(\d+)\D+(\d+)\D+/'){
+        preg_match_all($rep,$str,$arr);
+        if(isset($arr[$i][0])){
+            return $arr[$i][0];
+        }
+        return false;
     }
 }
--- test/tongcheng.php
+++ test/tongcheng.php
@@ -2,5 +2,10 @@
 require '../vendor/autoload.php';
 
  use ExplainHtml\TongCheng;
+// http://m.58.com/mm/yewu/27462874464970x.shtml
+// http://m.58.com/mm/yewu/28311250063170x.shtml
+$data = TongCheng::jobList('http://mm.58.com/yewu/');
+print_r($data);
 
- TongCheng::resumeInfo('1231321');
+$data = TongCheng::jobInfo('http://m.58.com/mm/yewu/28311250063170x.shtml');
+print_r($data);
